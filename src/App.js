209baa6cd78d21dee6c1169bdaf4a4ce40a4b9a2
Prince_Np <46887030+PrinceNiyonshuti@users.noreply.github.com>
2021-10-23 12:18:35 +0200

    added layout component to Parent JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import Layout from "./compoments/Layout";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>test Project </h1>
+    <div>
+      <Layout />
     </div>
   );
 }
--- src/compoments/Layout.js
+++ src/compoments/Layout.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function Layout() {
   return (

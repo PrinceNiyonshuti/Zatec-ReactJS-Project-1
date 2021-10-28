ea7cbf539595e07e2710b47b16700504c05ee5c6
Prince_Np <46887030+PrinceNiyonshuti@users.noreply.github.com>
2021-10-28 11:10:56 +0200

    added comments from input

--- src/compoments/Comment.js
+++ src/compoments/Comment.js
@@ -1,76 +1,102 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 
-class Comment extends Component {
-  constructor() {
-    super();
+const Comment = () => {
+  const commentsData = [
+    { id: 1, name: "Prince", details: "ello tere", posted_time: 2020 }
+  ];
 
-    this.state = {
-      imgHolder: "",
-      nameHolder: "",
-      message: ""
+  const [comments, setComments] = useState(commentsData);
+
+  /* Form variables */
+  const commentName = useRef();
+  const commentDetails = useRef();
+  const commentTime = useRef();
+  const commentForm = useRef();
+
+  const postComment = (event) => {
+    event.preventDefault();
+
+    const name = commentName.current.value;
+    const details = commentDetails.current.value;
+    const posted_time = commentTime.current.value;
+
+    const newComment = {
+      id: comments.length + 1,
+      name: name,
+      details: details,
+      posted_time: posted_time
     };
-    // binding function
-    this.postComment = this.postComment.bind(this);
-  }
 
-  // Post Comment Function
-  postComment() {
-    this.setState(() => {
-      return {
-        imgHolder: "https://source.unsplash.com/random/40x40",
-        nameHolder: "Prince",
-        message:
-          "Hey, Im here to help you out please tell me from the help team "
-      };
-    });
-  }
+    setComments([...comments, newComment]);
+    commentForm.current.reset();
+  };
 
-  render() {
-    return (
-      <div>
-        <h2 className="font-bold text-2xl mb-2">Comments</h2>
-        <div className="w-full p-4">
-          <ul>
-            <li className="flex justify-between items-center bg-white mt-2 p-2 shadow-lg rounded cursor-pointer">
+  return (
+    <div>
+      <h2 className="font-bold text-2xl mb-2">Comments</h2>
+      <div className="w-full p-4">
+        <ul>
+          {comments.map((comment) => (
+            <li
+              key={comment.id}
+              className="flex justify-between items-center bg-white mt-2 p-2 shadow-lg rounded cursor-pointer"
+            >
               <div className="flex ml-2">
-                <img
-                  src={this.state.imgHolder}
-                  className="rounded-full"
-                  alt={this.state.nameHolder}
-                />
                 <div className="flex flex-col ml-2">
-                  <span className="font-medium">{this.state.nameHolder}</span>
-                  <span className="text-sm ">{this.state.message}</span>
+                  <span className="font-medium">{comment.name}</span>
+                  <span className="text-sm ">{comment.details}</span>
                 </div>
               </div>
             </li>
-          </ul>
-        </div>
-        <h3 className="font-bold text-xl mb-2">Post Comment</h3>
-        <div class="flex">
+          ))}
+        </ul>
+      </div>
+      <h3 className="font-bold text-xl mb-2">Post Comment</h3>
+      <form onSubmit={postComment} ref={commentForm}>
+        <div className="flex">
           <div className="w-full bg-white p-2 rounded">
+            <div className=" p-3 w-full">
+              <input
+                type="text"
+                required
+                id="name"
+                ref={commentName}
+                className="border p-2 rounded w-full"
+                placeholder="Your Name"
+              />
+            </div>
+            <div className=" p-3 w-full">
+              <input
+                type="time"
+                required
+                id="posted_time"
+                ref={commentTime}
+                className="border p-2 rounded w-full"
+                placeholder="Your Name"
+              />
+            </div>
             <div className=" p-3 w-full">
               <textarea
                 rows="3"
+                id="name"
+                ref={commentDetails}
                 className="border p-2 rounded w-full"
                 placeholder="Write something..."
               />
             </div>
+
             <div className="flex justify-between mx-3">
               <div>
-                <button
-                  className="px-4 py-1 bg-blue-800 text-white rounded font-light hover:bg-blue-700"
-                  onClick={this.postComment}
-                >
+                <button className="px-4 py-1 bg-blue-800 text-white rounded font-light hover:bg-blue-700">
                   Post Comment
                 </button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-}
+      </form>
+    </div>
+  );
+};
 
 export default Comment;
